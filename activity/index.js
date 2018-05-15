7cd89a308621381705fa76bb930f6da74fbcaa7f
brandon blackwell <zilla.1234@hotmail.com>
2018-05-15 15:25:12 -0400

    user activity log

--- activity/index.js
+++ activity/index.js
@@ -3,10 +3,10 @@ const getTodaysDate = require('../helper_functions/getTodaysDate');
 const userCollection = require('../models/users');
 
 function activity(title, url, action, req, io) {
-    userCollection.findOneAndUpdate({ 'userId': req.user }, 
-    {$push: { 'activity': { title, url, action, date: getTodaysDate() } }}, {new: true}, (err, updated) => {
-       if(err) return err;
-    });
+    userCollection.findOneAndUpdate({ 'userId': req.user },
+        { $push: { 'activity': { title, url, action, date: getTodaysDate() } } }, { new: true }, (err, updated) => {
+            if (err) return err;
+        });
 }
 
 module.exports = activity;
