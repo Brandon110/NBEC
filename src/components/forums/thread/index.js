5ccaefaab555f26dcdf797d71538ba2504c6b885
brandon blackwell <zilla.1234@hotmail.com>
2018-05-17 12:14:13 -0400

    display thread comments

--- src/components/forums/thread/index.js
+++ src/components/forums/thread/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../../../actions/fetch_user';
 import Parser from 'html-react-parser';
 import Alert from '../../alerts/no_data';
 import AddComment from '../comments/add_comment';
+import DisplayComments from '../comments/display_comments';
 import { NavLink, Link } from 'react-router-dom';
 import axios from 'axios';
 
@@ -11,7 +14,7 @@ class Thread extends Component {
 
         this.state = {
             thread: null,
-            user: null
+            threadAuthor: null
         }
     }
 
@@ -21,7 +24,12 @@ class Thread extends Component {
 
     fetchThread() {
         axios.get('/thread/' + this.props.match.params.id).then(function (response) {
-            this.setState({ thread: response.data }, () => this.fetchUser());
+            this.setState({ thread: response.data }, () => {
+                ;
+                if (this.state.thread.status !== 'error') {
+                    this.fetchUser()
+                }
+            });
         }.bind(this))
             .catch(err => {
                 return err;
@@ -32,7 +40,7 @@ class Thread extends Component {
         let thread = this.state.thread;
 
         axios.get('/profile/live/' + thread.author.userId).then(function (response) {
-            this.setState({ user: response.data });
+            this.setState({ threadAuthor: response.data });
         }.bind(this))
             .catch(err => {
                 return err;
@@ -41,9 +49,10 @@ class Thread extends Component {
 
     renderThread() {
         let thread = this.state.thread;
-        let user = this.state.user;
+        let threadAuthor = this.state.threadAuthor;
+        let user = this.props.user;
 
-        if (!thread || !user) {
+        if (!thread) {
             return false;
         }
         else if (thread.status === 'error') {
@@ -55,29 +64,45 @@ class Thread extends Component {
 
                     <h1 className='text-center'>{thread.title}</h1>
 
-                    <div className='d-flex white-background flex-1 p-1 thread-wrapper border'>
-
-                        <div className='d-flex flex-column align-items-center mr-2'>
-                            <img className='profile-img' src={user.profileImg} />
-                            <div><NavLink to={'/live-profile/activity/' + user.userId}>{user.firstName + ' ' + user.lastName}</NavLink></div>
-                            <div><small className='text-muted'>Born <strong>{user.birthDate}</strong></small></div>
-                            <div><small className='text-muted'>Posted <strong>{thread.datePosted}</strong></small></div>
-                            <div className='d-flex mt-1'>
-                                <small className='mr-2'>Likes {thread.likes.length}</small>
-                                <small>Replies {thread.comments.length}</small>
-                            </div>
-                        </div>
-
-                        <div className='d-flex flex-column align-items-center'>
+                    <div className='d-flex flex-1 wrap-on-resize border white-background'>
+                        {
+                            threadAuthor ?
+                                <div className='mr-2'>
+                                    <div className='d-flex flex-column align-items-center p-1' style={{background: '#f2f2f2'}}>
+                                    <img className='profile-img' src={threadAuthor.profileImg} />
+                                    <div><NavLink to={'/live-profile/activity/' + threadAuthor.userId}>{threadAuthor.firstName + ' ' + threadAuthor.lastName}</NavLink></div>
+                                    <div><small className='text-muted'>Born <strong>{threadAuthor.birthDate}</strong></small></div>
+                                    <div><small className='text-muted'>Posted <strong>{thread.datePosted}</strong></small></div>
+                                    <div className='d-flex mt-1'>
+                                        <small className='mr-2'>Likes {thread.likes.length}</small>
+                                        <small>Replies {thread.comments.length}</small>
+                                    </div>
+                                    </div>
+                                </div>
+                                :
+                                false
+                        }
+                        <section className='p-1'>
                             {Parser(thread.body)}
-                        </div>
+                        </section>
                     </div>
 
                     <div className='mt-5'>
                         <h5>Comments {thread.comments.length}</h5>
-                        <AddComment 
-                        thread={thread} 
-                        fetchThread={this.fetchThread.bind(this)}
+                        {
+                            user && !user.loading ?
+                                <AddComment
+                                    thread={thread}
+                                    fetchThread={this.fetchThread.bind(this)}
+                                />
+                                :
+                                <small>Please <NavLink to='/signin'>Sign in</NavLink> to comment</small>
+                        }
+                    </div>
+
+                    <div className='mt-5 mb-5'>
+                        <DisplayComments
+                            comments={thread.comments}
                         />
                     </div>
                 </div>
@@ -95,4 +120,10 @@ class Thread extends Component {
     }
 }
 
-export default Thread;
+const mapStateToProps = (state) => {
+    return {
+        user: state.user
+    }
+}
+ 
+export default connect(mapStateToProps, null)(Thread);
