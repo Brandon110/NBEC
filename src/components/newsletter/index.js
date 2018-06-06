b2fcbf138b384679daf5273be01a822b8670a0d9
brandon blackwell <zilla.1234@hotmail.com>
2018-06-06 17:13:02 -0400

    removed unnecessary css

--- src/components/newsletter/index.js
+++ src/components/newsletter/index.js
@@ -6,14 +6,12 @@ class NewsLetter extends Component {
     render() {
         return (
             <main>
-                <div className='newsletter-description-wrapper'>
+                <div className='newsletter-description-wrapper grey-border-bottom'>
                     <div className='d-flex flex-column justify-content-center align-items-center'>
-                        <div className='white-border-bottom w-50 pb-3 d-flex justify-content-center'>
-                            <i className="fas fa-envelope-square"></i>
-                        </div>
-                        <div className='pt-3 text-center'>
-                            <p className='newsletter-description w-75'>
-                                Subscribe to our mailing list to receive regular emails regarding new environmental dicscussions and more!
+                        <i className="fas fa-envelope-square"></i>
+                        <div className='mt-3'>
+                            <p className='text-center'>
+                                Sign up to NBEC mailing list to receive regular emails regarding new environmental diiscussions and more!
             </p>
                         </div>
                     </div>
