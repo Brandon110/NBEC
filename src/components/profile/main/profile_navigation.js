9011b0dd027de7c083298ce6a951ba670d59e9b0
brandon blackwell <zilla.1234@hotmail.com>
2018-06-06 12:24:58 -0400

    imporved profile navigation

--- src/components/profile/main/profile_navigation.js
+++ src/components/profile/main/profile_navigation.js
@@ -4,7 +4,7 @@ import { NavLink, Link } from 'react-router-dom';
 class ProfileNavigation extends Component {
     render() {
         return <nav className='d-flex align-items-center mt-2 mb-2 profile-header'>
-            <NavLink className='header-link' exact to='/profile/activity' activeClassName='active'>Activity</NavLink>
+            <NavLink className='header-link ml-3' exact to='/profile/activity' activeClassName='active'>Activity</NavLink>
             <NavLink className='header-link' to='/profile/posts' activeClassName='active'>Forum Posts</NavLink>
             <NavLink className='header-link' to='/profile/update-profile' activeClassName='active'>Settings</NavLink>
         </nav>
