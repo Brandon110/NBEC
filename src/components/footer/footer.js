975698000b50fe64eb06386fd455f45613b654b6
brandon blackwell <zilla.1234@hotmail.com>
2018-06-07 11:17:33 -0400

    changed footer color

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import './footer.css';
-
+ 
 class Footer extends Component {
     render() {
         return (
-           <footer>
-           
-           <p className='text-center'>&copy;2018, by Brandon Blackwell</p>
-           </footer>
+            <footer className='d-flex align-items-center justify-content-center p-4' style={{backgroundColor: '#99ff99'}}>
+                    <p className='p-0 m-0'>&copy;2018, by Brandon Blackwell</p>
+            </footer>
         )
     }
 }
