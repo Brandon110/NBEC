d4fedf5be6ddf7941224ce736dbc54486800679a
brandon blackwell <zilla.1234@hotmail.com>
2018-06-07 11:20:05 -0400

    credit to google news api

--- src/components/news/index.js
+++ src/components/news/index.js
@@ -8,8 +8,8 @@ class NewsPage extends Component {
         return (
             <main>
                 <div className='container-fluid mt-5 mb-5'>
-                    <div className='box-title'>
-                        <p>News and Articles</p>
+                    <div className='box-title p-3 mb-5'>
+                        <p className='p-0 m-0'>News and Articles - powered by <a href='https://newsapi.org/s/google-news-api' target='__blank'>Google News Api</a></p>
                     </div>
                     <Headlines />
                     <AllNews />
